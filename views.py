bee7ce67722b782286afee9ebb7c7eee8304880d
Miłosz Cur <miloszcur123@gmail.com>
2024-04-08 11:02:58 +0200

    wszystko

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-<<<<<<< HEAD
+
 from main import products
 
 views = Blueprint(__name__,"views")
@@ -8,11 +8,4 @@ views = Blueprint(__name__,"views")
 def home():
     return render_template("index.html", products=products)
 
-=======
 
-views = Blueprint(__name__, "views")
-
-@views.route("/")
-def home():
-    return render_template("index.html")
->>>>>>> 971d77e30fcdaf8951b730a780c33b46d59585da
